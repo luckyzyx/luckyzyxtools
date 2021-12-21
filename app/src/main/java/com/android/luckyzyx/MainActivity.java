9a4721d5940cb78e2b822a221267f3e8288187d2
luckyzyx <2813467705@qq.com>
2021-12-21 16:18:49 +0800

    modify theme

--- app/src/main/java/com/android/luckyzyx/MainActivity.java
+++ app/src/main/java/com/android/luckyzyx/MainActivity.java
@@ -1,8 +1,15 @@
 package com.android.luckyzyx;
 
+import static android.widget.Toast.*;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn1 = findViewById(R.id.button);
+        btn1.setOnClickListener(v -> makeText(this, "Toast: 点击按钮", LENGTH_SHORT).show());
+        Button btn2 = findViewById(R.id.button2);
+        btn2.setOnClickListener(v -> {
+            AlertDialog.Builder alert = new AlertDialog.Builder(this)
+                    .setTitle("标题")
+                    .setMessage("消息")
+                    .setCancelable(true)
+                    .setPositiveButton("确定", (dialog, which) -> {
+                        Toast.makeText(this,"点击确定",LENGTH_SHORT).show();
+                    })
+                    .setNegativeButton("取消",  (dialog, which) -> {
+                        Toast.makeText(this,"点击取消",LENGTH_SHORT).show();
+                    })
+                    .setNeutralButton("中间",  (dialog, which) -> {
+                        Toast.makeText(this,"点击中间",LENGTH_SHORT).show();
+                    });
+            alert.create().show();
+        });
     }
 }
