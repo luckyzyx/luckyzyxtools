bc054d46891404757e07d4e5c2806aec93ee4830
luckyzyx <2813467705@qq.com>
2021-12-22 21:55:33 +0800

    modify build.gradle and proguard-rules.pro

--- app/build/generated/source/buildConfig/debug/com/android/luckyzyx/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/android/luckyzyx/BuildConfig.java
@@ -4,11 +4,9 @@
 package com.android.luckyzyx;
 
 public final class BuildConfig {
-  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final boolean DEBUG = true;
   public static final String APPLICATION_ID = "com.android.luckyzyx";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
-  // Field from build type: debug
-  public static final Boolean DEBUG_MODE = true;
 }
