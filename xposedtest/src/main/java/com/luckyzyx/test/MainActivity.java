f4485530b4def28b103b385feac5399cafd32b81
luckyzyx <2813467705@qq.com>
2022-02-20 12:49:53 +0800

    Xposed Test APP替换MD3

--- xposedtest/src/main/java/com/luckyzyx/test/MainActivity.java
+++ xposedtest/src/main/java/com/luckyzyx/test/MainActivity.java
@@ -3,7 +3,6 @@ package com.luckyzyx.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
